64fa32bc2c08eefca60d7ba0bb829ef76560a47a
Izan <izanmajeed03@gmail.com>
2021-02-10 13:54:38 +0530

    Data Structure Class added

---
+++ data_structure.dart
@@ -0,0 +1,16 @@
+class Word {
+  String title;
+  String pos;
+  String englishMeaning;
+  String kashmiriMeaning;
+  String englishExample;
+  String kashmiriExample;
+  Word({
+    this.title,
+    this.pos,
+    this.englishMeaning,
+    this.kashmiriMeaning,
+    this.englishExample,
+    this.kashmiriExample,
+  });
+}
