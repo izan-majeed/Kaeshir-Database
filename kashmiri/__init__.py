d6f5171f70f3f90023613f49d4bf1452de3f1893
Izan Majeed <izanmajeed03@gmail.com>
2021-05-16 18:04:45 +0530

    init updated

--- kashmiri/__init__.py
+++ kashmiri/__init__.py
@@ -1,2 +1,9 @@
 from kashmiri.kashmiri import find
 from kashmiri.database import data
+
+print('''
+author:    "Izan Majeed Mir"
+email:     "izan_28btech17@nitsri.net"
+license:   "Apache License 2.0"
+version:   "0.0.1"
+''')
